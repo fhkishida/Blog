43d5941865c096475e8aa9de9da045408ac796e3
fabricio <fabricio@4x4brasil.com.br>
2022-01-31 13:51:02 -0300

    feature search on homepage

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
@@ -17,21 +18,9 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 |
 */
 
-Route::get('/', function () {
-    
-    return view('posts', [
-        'posts' =>  Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-})->name('home');
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-Route::get('/posts/{post}', function(Post $post){
-
-    return view('post', 
-        [ 'post' => $post ]
-    );
-
-});
+Route::get('/posts/{post}', [PostController::class, 'show']);
 
 Route::get('/categories/{category:slug}', function(Category $category){
 
